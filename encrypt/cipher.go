3e7ade28aa180e13f3c3a5eb95ddf07b38f644f6
Fernando Castro <fernando.castro@telco.com.ar>
2021-08-09 15:52:08 -0300

    add encrypt and decrypt streams

--- encrypt/cipher.go
+++ encrypt/cipher.go
@@ -11,30 +11,61 @@ import (
 	"io"
 )
 
-func Encrypt(key, plaintext string) (string, error) {
+func encryptStream(key string, iv []byte) (cipher.Stream, error) {
 	block, err := newCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
+	return cipher.NewCFBEncrypter(block, iv), nil
+}
 
+// Encrypt will take in a key and plaintext and return hex representation
+// of encrypted value.
+// This code is based on the standard library example in
+// - https://golang.org/pkg/crypto/cipher/#NewCFBEncrypter
+func Encrypt(key, plaintext string) (string, error) {
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 		return "", err
 	}
 
-	stream := cipher.NewCFBEncrypter(block, iv)
+	stream, err := encryptStream(key, iv)
+	if err != nil {
+		return "", err
+	}
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
 
 	return fmt.Sprintf("%x\n", ciphertext), nil
 }
 
-func Decrypt(key, cipherHex string) (string, error) {
+// EncryptWriter will return a writer that will write encrypted data to
+// the original writer.
+func EncryptWriter(key string, w io.Writer) (*cipher.StreamWriter, error) {
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	stream, err := encryptStream(key, iv)
+	if err != nil {
+		return nil, err
+	}
+	n, err := w.Write(iv)
+	if n != len(iv) || err != nil {
+		return nil, errors.New("encrypt: unable to write full iv to writer")
+	}
+	return &cipher.StreamWriter{S: stream, W: w}, nil
+}
+
+func decryptStream(key string, iv []byte) (cipher.Stream, error) {
 	block, err := newCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
+	return cipher.NewCFBDecrypter(block, iv), nil
+}
 
+func Decrypt(key, cipherHex string) (string, error) {
 	ciphertext, err := hex.DecodeString(cipherHex)
 	if err != nil {
 		return "", err
@@ -46,11 +77,28 @@ func Decrypt(key, cipherHex string) (string, error) {
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
 
-	stream := cipher.NewCFBDecrypter(block, iv)
+	stream, err := decryptStream(key, iv)
+	if err != nil {
+		return "", err
+	}
 
 	stream.XORKeyStream(ciphertext, ciphertext)
 
 	return string(ciphertext), nil
+}
+
+// DecryptReader will return a reader that will decrypt data from the provided reader
+func DecryptReader(key string, r io.Reader) (*cipher.StreamReader, error) {
+	iv := make([]byte, aes.BlockSize)
+	n, err := r.Read(iv)
+	if n < len(iv) || err != nil {
+		return nil, errors.New("encrypt: unable to read the full iv")
+	}
+	stream, err := decryptStream(key, iv)
+	if err != nil {
+		return nil, err
+	}
+	return &cipher.StreamReader{S: stream, R: r}, nil
 }
 
 func newCipher(key string) (cipher.Block, error) {
