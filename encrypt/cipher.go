0443a0dfb7d0ce9339fe1d803f8dc0b4f7907df3
Fernando Castro <fernando.castro@telco.com.ar>
2021-08-09 09:24:55 -0300

    complete encrypt and decrypt functions

--- encrypt/cipher.go
+++ encrypt/cipher.go
@@ -6,6 +6,7 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 )
@@ -25,38 +26,31 @@ func Encrypt(key, plaintext string) (string, error) {
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
 
-	// It's important to remember that ciphertexts must be authenticated
-	// (i.e. by using crypto/hmac) as well as being encrypted in order to
-	// be secure.
 	return fmt.Sprintf("%x\n", ciphertext), nil
 }
 
-func Decrypt() {
-	// Load your secret key from a safe place and reuse it across multiple
-	// NewCipher calls. (Obviously don't use this example key for anything
-	// real.) If you want to convert a passphrase to a key, use a suitable
-	// package like bcrypt or scrypt.
-	key, _ := hex.DecodeString("6368616e676520746869732070617373")
-	ciphertext, _ := hex.DecodeString("7dd015f06bec7f1b8f6559dad89f4131da62261786845100056b353194ad")
-
-	block, err := aes.NewCipher(key)
+func Decrypt(key, cipherHex string) (string, error) {
+	block, err := newCipher(key)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
+	ciphertext, err := hex.DecodeString(cipherHex)
+	if err != nil {
+		return "", err
+	}
+
 	if len(ciphertext) < aes.BlockSize {
-		panic("ciphertext too short")
+		return "", errors.New("Encrypt: ciphertext too short")
 	}
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
 
 	stream := cipher.NewCFBDecrypter(block, iv)
 
-	// XORKeyStream can work in-place if the two arguments are the same.
 	stream.XORKeyStream(ciphertext, ciphertext)
-	fmt.Printf("%s", ciphertext)
+
+	return string(ciphertext), nil
 }
 
 func newCipher(key string) (cipher.Block, error) {
